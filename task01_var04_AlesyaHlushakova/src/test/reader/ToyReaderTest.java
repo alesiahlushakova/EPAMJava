8d5c1eaeb63bd9d25e09cf27b195bd73f799c371
alesiahlushakova <aleskahlushakova@gmail.com>
2019-05-15 19:56:25 +0300

    last checks

--- task01_var04_AlesyaHlushakova/src/test/reader/ToyReaderTest.java
+++ task01_var04_AlesyaHlushakova/src/test/reader/ToyReaderTest.java
@@ -15,28 +15,33 @@ import java.util.List;
 
 public class ToyReaderTest {
     private static final Logger LOGGER = LogManager.getLogger(ToyParserTest.class);
-    String toy1, toy2, toy3, toy4, toy5, toy6, toy7;
-    ToyReader toyReader1, toyReader2, toyReader3;
+    String toy1, toy2, toy3, toy4, toy5, toy6, toy7, source, source1, source2;
+    ToyReader toyReader = new ToyReader();
+    LinkedList<String> toys, toys1, toys2;
     @BeforeClass
     public void setBefore() {
-        String toy1 = "type=BALL name=Bolick price=20 size=SMALL age=BABY BALL_TYPE=TENNIS";
-        String toy2 = "type=DOLL name=Mashka price=15 size=MEDIUM age=CHILD DOLL_TYPE=BARBIE";
-        String toy3 = "type=CUBE name=Borya price=230 size=LARGE age=TEENAGER CUBE_TYPE=CLASSIC";
-        String toy4 = "type=VEHICLE name=Winston3000 price=299 size=SMALL age=TEENAGER VEHICLE_TYPE=TRUCK";
-        String toy5 = "type=VEHICLE name=Monster price=170 size=LARGE age=CHILD VEHICLE_TYPE=TRUCK";
-        String toy6 = "type=DOLL name=MND-198 price=200 size=SMALL age=TEENAGER DOLL_TYPE=PORCELIAN";
-        String toy7 ="type=BALL name=alenka price=25 size=SMALL age=TEENAGER BALL_TYPE=TENNIS";
-        toyReader1 = new ToyReader();
-        toyReader2 = new ToyReader();
+        toy1 = "type=BALL name=Borya price=230 size=SMALL age=BABY BALL_TYPE=TENNIS";
+        toy2 = "type=CUBE name=Borya price=30 size=LARGE age=TEENAGER CUBE_TYPE=CLASSIC";
+        toy3 = "type=VEHICLE name=Winston3000 price=299 size=SMALL age=TEENAGER VEHICLE_TYPE=TRUCK";
+        toy4 = "type=VEHICLE name=Monster price=170 size=LARGE age=CHILD VEHICLE_TYPE=TRUCK";
+        toy5 = "type=DOLL name=MND-198 price=210 size=SMALL age=TEENAGER DOLL_TYPE=PORCELIAN";
+        toy6 = "type=DOLL name=Mashka price=15 size=MEDIUM age=CHILD DOLL_TYPE=BARBIE";
+        toy7 ="type=BALL name=alenka price=25 size=SMALL age=TEENAGER BALL_TYPE=TENNIS";
+        source = "data\\toys1.txt";
+        source1 = "data\\toys.txt";
+        source2 = "data\\empty.txt";
+        toys = new LinkedList<>(Arrays.asList(toy1, toy2,toy3,toy4, toy5, toy6)) ;
+        toys1 = new LinkedList<>(Arrays.asList(toy7));
+        toys2 = new LinkedList<>();
     }
     @DataProvider(name = "provider")
     public Object[][] dataProviderTestReader() {
-        return new Object[][]{{toyReader1, Arrays.asList(toy1, toy2,toy3,toy4, toy5, toy6)}, {toyReader2, Arrays.asList(toy7)}};
+        return new Object[][]{{toyReader, toys, source}, {toyReader, toys1, source1}, {toyReader, toys2, source2}};
     }
     @Test(dataProvider = "provider")
-    public void testRead(ToyReader toyReader, LinkedList<String> expected) {
+    public void testRead(ToyReader toyReader, LinkedList<String> expected, String source) {
 
-        List<String> actual = toyReader.read("data\\toys1.txt");
+        LinkedList<String> actual = toyReader.read(source);
         Assert.assertEquals(actual, expected);
     }
 }
